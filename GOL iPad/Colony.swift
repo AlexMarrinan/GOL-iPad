bb083dccbb1d316da787e344a0c191ff33c43ee7
Alex Marrinan <alex@balroe.com>
2019-11-08 13:04:47 -0500

    idk

---
+++ GOL iPad/Colony.swift
@@ -0,0 +1,21 @@
+//
+//  Colony.swift
+//  GOL iPad
+//
+//  Created by Alex Marrinan on 11/8/19.
+//  Copyright © 2019 Alex Marrinan. All rights reserved.
+//
+
+import SwiftUI
+
+struct Colony: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Colony_Previews: PreviewProvider {
+    static var previews: some View {
+        Colony()
+    }
+}
