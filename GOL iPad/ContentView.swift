869778dc9629d36459e9de1f2b345ba113a9090f
Alex Marrinan <alex@balroe.com>
2019-11-08 12:51:57 -0500

    Initial Commit

---
+++ GOL iPad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GOL iPad
+//
+//  Created by Alex Marrinan on 11/8/19.
+//  Copyright © 2019 Alex Marrinan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
